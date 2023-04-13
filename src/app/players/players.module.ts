08ea59f7d9a389e3bb17f92aa1a8886decf173fa
Douglas Vieira <douglasdotv@hotmail.com>
2023-04-13 13:41:22 -0300

    Organize imports

--- src/app/players/players.module.ts
+++ src/app/players/players.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 import { PlayersRoutingModule } from './players-routing.module';
 import { PlayersComponent } from './players/players.component';
