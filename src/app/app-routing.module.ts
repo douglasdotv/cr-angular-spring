7c85dd8d574133a9b4072d0a0abc3219211025af
Douglas Vieira <douglasdotv@hotmail.com>
2023-04-13 12:54:38 -0300

    Configure lazy-loading for PlayersModule in app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'players', pathMatch: 'full' },
+  {
+    path: 'players',
+    loadChildren: () => import('./players/players.module').then(m => m.PlayersModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
