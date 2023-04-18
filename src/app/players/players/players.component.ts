b1ad1d649ef995d59cdb4ca215a9ace5828a32de
Douglas Vieira <douglasdotv@hotmail.com>
2023-04-18 11:36:40 -0300

    Initialize players array in PlayersComponent

--- src/app/players/players/players.component.ts
+++ src/app/players/players/players.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Player } from '../model/player';
 
 @Component({
   selector: 'app-players',
   templateUrl: './players.component.html',
   styleUrls: ['./players.component.scss']
 })
-export class PlayersComponent {
+export class PlayersComponent implements OnInit {
+
+    players: Player[];
+
+    constructor() {
+      this.players = [];
+    }
+
+    ngOnInit(): void {
+      // nothing to do
+    }
 
 }
