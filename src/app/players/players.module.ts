acc41d02c7eb2ef72a6938257d6c8a2e9a819ffe
Douglas Vieira <douglasdotv@hotmail.com>
2023-04-18 11:11:45 -0300

    Import MatTableModule

--- src/app/players/players.module.ts
+++ src/app/players/players.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 
 import { PlayersRoutingModule } from './players-routing.module';
 import { PlayersComponent } from './players/players.component';
@@ -11,7 +12,8 @@ import { PlayersComponent } from './players/players.component';
   ],
   imports: [
     CommonModule,
-    PlayersRoutingModule
+    PlayersRoutingModule,
+    MatTableModule
   ]
 })
 export class PlayersModule { }
