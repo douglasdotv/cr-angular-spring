a9bc9e9cf2b7d2f59162ea9eef8b908525020e3f
Douglas Vieira <douglasdotv@hotmail.com>
2023-04-21 10:17:39 -0300

    Create shared AppMaterialModule for imports

--- src/app/players/players.module.ts
+++ src/app/players/players.module.ts
@@ -1,9 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatTableModule } from '@angular/material/table';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { PlayersRoutingModule } from './players-routing.module';
 import { PlayersComponent } from './players/players.component';
 
@@ -14,9 +12,7 @@ import { PlayersComponent } from './players/players.component';
   imports: [
     CommonModule,
     PlayersRoutingModule,
-    MatTableModule,
-    MatCardModule,
-    MatToolbarModule
+    AppMaterialModule
   ]
 })
 export class PlayersModule { }
