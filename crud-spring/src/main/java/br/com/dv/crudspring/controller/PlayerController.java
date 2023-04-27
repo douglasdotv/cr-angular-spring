30997a7637f7241d2c73feab7756cda0a9077288
Douglas Vieira <douglasdotv@hotmail.com>
2023-04-27 00:33:56 -0300

    Implement endpoints to get all teams/get all players

--- crud-spring/src/main/java/br/com/dv/crudspring/controller/PlayerController.java
+++ crud-spring/src/main/java/br/com/dv/crudspring/controller/PlayerController.java
@@ -1,16 +1,24 @@
 package br.com.dv.crudspring.controller;
 
+import br.com.dv.crudspring.model.Player;
+import br.com.dv.crudspring.repository.PlayerRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@AllArgsConstructor
 @RestController
 @RequestMapping("/api/players")
 public class PlayerController {
 
+    private final PlayerRepository playerRepository;
+
     @GetMapping
-    public String player() {
-        return "players";
+    public List<Player> getPlayers() {
+        return playerRepository.findAll();
     }
 
 }
--- crud-spring/src/main/java/br/com/dv/crudspring/controller/TeamController.java
+++ crud-spring/src/main/java/br/com/dv/crudspring/controller/TeamController.java
@@ -1,16 +1,24 @@
 package br.com.dv.crudspring.controller;
 
+import br.com.dv.crudspring.model.Team;
+import br.com.dv.crudspring.repository.TeamRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@AllArgsConstructor
 @RestController
 @RequestMapping("/api/teams")
 public class TeamController {
 
+    private final TeamRepository teamRepository;
+
     @GetMapping
-    public String teams() {
-        return "team";
+    public List<Team> getTeams() {
+        return teamRepository.findAll();
     }
 
 }
